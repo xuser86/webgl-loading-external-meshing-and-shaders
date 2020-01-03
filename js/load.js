bbdf26a419ecfec0f156303715e511a11afa1d65
Rafał Jeżewski <rafal.jezewski@gmail.com>
2020-01-03 19:16:24 +0100

    finished tut

--- js/load.js
+++ js/load.js
@@ -1,12 +1,26 @@
 // Load text resource ower the network
-function loadResource(url) {
+function loadResource(url, type = 'TEXT') {
   return fetch(url)
     .then((response) => {
-      if (response.status < 200 || response.status > 299) {
-        throw new Error(response.statusText + ' : ' + url);
-      } else {
-        return response.json();
-      }
+        if (response.status < 200 || response.status > 299) {
+            throw new Error(response.statusText + ' : ' + url);
+        } else {
+            if ( type === 'TEXT') {
+                return response.text();
+            } else if ( type === 'JSON') {
+                return response.json();
+            } else if ( type === 'IMAGE') {
+                return new Promise((resolve) => {
+                  const image = new Image();
+                  image.onload = () => {
+                    resolve(image);
+                  }
+                  image.src = url;
+                });
+            } else {
+                throw new Error('Error unknown resource type : ', type);
+            }
+        }
     })
     .catch((error) => {
         throw new Error(error);
--- main.js
+++ main.js
@@ -1,34 +1,5 @@
-var vertexShaderText = [
-  'precision mediump float;',
-  '',
-  'attribute vec3 vertPosition;',
-  'attribute vec2 vertTexCoord;',
-  'varying vec2 fragTexCoord;',
-  'uniform mat4 mWorld;',
-  'uniform mat4 mView;',
-  'uniform mat4 mProj;',
-  '',
-  'void main()',
-  '{',
-  ' fragTexCoord = vertTexCoord;',
-  ' gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);',
-  '}'
-].join('\n');
-
-var fragmentShaderText = [
-  'precision mediump float;',
-  '',
-  'varying vec2 fragTexCoord;',
-  'uniform sampler2D sampler;',
-  '',
-  'void main()',
-  '{',
-  '  gl_FragColor = texture2D(sampler, fragTexCoord);',
-  '}'
-].join('\n');
-
 var gl;
-var InitDemo = function() {
+var RunDemo = function(vertexShaderText, fragmentShaderText, model, texture) {
   const canvas = document.getElementById("canvasGL");
   gl = canvas.getContext('webgl');
   if (!gl) {
@@ -78,98 +49,42 @@ var InitDemo = function() {
   }
   
   /* create buffer */
-  const boxVerticles = [ 
-    // X, Y, Z,        U, V
-		// Top
-		-1.0, 1.0, -1.0,   0, 0,
-		-1.0, 1.0, 1.0,    0, 1,
-		1.0, 1.0, 1.0,     1, 1,
-		1.0, 1.0, -1.0,    1, 0,
-
-		// Left
-		-1.0, 1.0, 1.0,    0, 0,
-		-1.0, -1.0, 1.0,   1, 0,
-		-1.0, -1.0, -1.0,  1, 1,
-		-1.0, 1.0, -1.0,   0, 1,
-
-		// Right
-		1.0, 1.0, 1.0,    1, 1,
-		1.0, -1.0, 1.0,   0, 1,
-		1.0, -1.0, -1.0,  0, 0,
-		1.0, 1.0, -1.0,   1, 0,
-
-		// Front
-		1.0, 1.0, 1.0,    1, 1,
-		1.0, -1.0, 1.0,    1, 0,
-		-1.0, -1.0, 1.0,    0, 0,
-		-1.0, 1.0, 1.0,    0, 1,
-
-		// Back
-		1.0, 1.0, -1.0,    0, 0,
-		1.0, -1.0, -1.0,    0, 1,
-		-1.0, -1.0, -1.0,    1, 1,
-		-1.0, 1.0, -1.0,    1, 0,
-
-		// Bottom
-		-1.0, -1.0, -1.0,   1, 1,
-		-1.0, -1.0, 1.0,    1, 0,
-		1.0, -1.0, 1.0,     0, 0,
-		1.0, -1.0, -1.0,    0, 1,
-  ];
-  
-  const boxIndices = [
-		// Top
-		0, 1, 2,
-		0, 2, 3,
-
-		// Left
-		5, 4, 6,
-		6, 4, 7,
-
-		// Right
-		8, 9, 10,
-		8, 10, 11,
-
-		// Front
-		13, 12, 14,
-		15, 14, 12,
-
-		// Back
-		16, 17, 18,
-		16, 18, 19,
-
-		// Bottom
-		21, 20, 22,
-		22, 20, 23
-	];
+  const boxVerticles = model.meshes[0].vertices;
+  const boxIndices = [].concat.apply([], model.meshes[0].faces);
+  const texCoords = model.meshes[0].texturecoords[0];
   
   const boxVertexBufferObject = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVerticles), gl.STATIC_DRAW);
   
+  const texCoordVertexBufferObject = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, texCoordVertexBufferObject);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texCoords), gl.STATIC_DRAW);
+
   const boxIndexBufferObject = gl.createBuffer();
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
   
+  gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject);
   const positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
-  const texCoordAttribLocation = gl.getAttribLocation(program, 'vertTexCoord');
-  //const colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
   gl.vertexAttribPointer(
     positionAttribLocation, // Attribute location
     3, // Number of elements per attribute
     gl.FLOAT, // Type of elements
     gl.FALSE,
-    5 * Float32Array.BYTES_PER_ELEMENT, // Size of invidual vertex
+    3 * Float32Array.BYTES_PER_ELEMENT, // Size of invidual vertex
     0 // Offset from the beginning of a single vertex to this attribute
   );
-  
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, texCoordVertexBufferObject);
+  const texCoordAttribLocation = gl.getAttribLocation(program, 'vertTexCoord');
   gl.vertexAttribPointer(
     texCoordAttribLocation, // Attribute location
     2, // Number of elements per attribute
     gl.FLOAT, // Type of elements
     gl.FALSE,
-    5 * Float32Array.BYTES_PER_ELEMENT, // Size of invidual vertex
-    3 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
+    2 * Float32Array.BYTES_PER_ELEMENT, // Size of invidual vertex
+    0 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
   );
   
   gl.enableVertexAttribArray(positionAttribLocation);
@@ -178,13 +93,14 @@ var InitDemo = function() {
   /** Create texture */
   const boxTexture = gl.createTexture();
   gl.bindTexture(gl.TEXTURE_2D, boxTexture);
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
   gl.texImage2D(
     gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE,
-    document.getElementById('texture-img')
+    texture
   );
   gl.bindTexture(gl.TEXTURE_2D, null);
  
@@ -243,4 +159,10 @@ var InitDemo = function() {
   
   
 }
-document.body.onload = InitDemo;
+document.body.onload = async () => {
+  const vertexShaderText = await loadResource('./res/shader.vs.glsl'); 
+  const fragmentShaderText = await loadResource('./res/shader.fs.glsl');
+  const model = await loadResource('./res/Susan.json', 'JSON');
+  const texture = await loadResource('./res/SusanTexture.png', 'IMAGE');
+  RunDemo(vertexShaderText, fragmentShaderText, model, texture);
+}
